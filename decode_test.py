aa2bdfe02394bd00c5e252286c4242a725a83758
unknown <danialmutalib.s@gmail.com>
2025-09-07 19:28:09 +0800

    changed directories

--- decode_test.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-def decode_pass(password):
-    decode_bytes = base64.b64decode(password)
-    decode_data = decode_bytes.decode()
-    print(f"decoded password {decode_data}")
-
-encode_string = input("Enter encoded password: ")
-decode_pass(encode_string)
--- encryption_test.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-def encrypt_pass(password):
-    encoded_bytes = base64.b64encode(password.encode())
-    print(encoded_bytes)
-
-user_pass = input("Enter your password: ")
-encrypt_pass(user_pass)
